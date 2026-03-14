c282eccb2a621e21dfd6f3a138c52c8065ab7c6d
ivonaaaa <ivonaa.e@gmail.com>
2026-03-14 13:56:21 +0100

    Style the collection inspired interior section

--- web/src/modules/product/collection-inspired-interior/CollectionInspiredInterior.tsx
+++ web/src/modules/product/collection-inspired-interior/CollectionInspiredInterior.tsx
@@ -2,16 +2,34 @@
 
 export default function CollectionInspiredInterior() {
   return (
-    <section>
-      <h1>Collection Inspired Interior</h1>
-      <img src="/images/CollectionInspiredInterior1.png" alt="sofa name" />
-      <img src="/images/CollectionInspiredInterior2.png" alt="sofa name" />
-      <div className="">
-        <img src="/images/CollectionInspiredInterior3.png" alt="sofa name" />
-        <h1>
-          The Paloma Haven sofa is a masterpiece of minimalism and luxury.
-        </h1>
-        <a href="">See more out of 'Modern Luxe' collection</a>
+    <section className="mt-48">
+      <h1 className="text-5xl font-medium mb-6">
+        Collection Inspired Interior
+      </h1>
+      <img
+        src="/images/CollectionInspiredInterior1.png"
+        alt="sofa name"
+        className="w-full"
+      />
+      <img
+        src="/images/CollectionInspiredInterior2.png"
+        alt="sofa name"
+        className="w-screen max-w-none -mx-24 my-20"
+      />
+      <div className="flex gap-28">
+        <img
+          src="/images/CollectionInspiredInterior3.png"
+          alt="sofa name"
+          className="w-5/12"
+        />
+        <div className="w-7/12">
+          <h1 className="text-5xl leading-normal font-medium mt-20 mb-6 pr-20">
+            The Paloma Haven sofa is a masterpiece of minimalism and luxury.
+          </h1>
+          <a href="" className="text-2xl underline">
+            See more out of 'Modern Luxe' collection
+          </a>
+        </div>
       </div>
     </section>
   )
