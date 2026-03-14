6c8cd9f0044beaa8e5e32d441bcb1abb9c87d36a
ivonaaaa <ivonaa.e@gmail.com>
2026-03-14 18:43:54 +0100

    Add content for footer

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,7 @@ import Navigation from "@modules/navigation/Navigation"
 import ProductCard from "@modules/product/product-card/ProductCard"
 import CollectionInspiredInterior from "@modules/product/collection-inspired-interior/CollectionInspiredInterior"
 import RelatedProducts from "@modules/product/related-products/RelatedProducts"
+import Footer from "@modules/footer/Footer"
 
 export default function ProductPage() {
   return (
@@ -12,6 +13,7 @@ export default function ProductPage() {
         <CollectionInspiredInterior />
         <RelatedProducts />
       </main>
+      <Footer />
 
       {/* sitit se ovu visinu maknit nakraju */}
       <div className="h-80"></div>
--- web/src/modules/footer/Footer.tsx
+++ web/src/modules/footer/Footer.tsx
@@ -1,5 +1,50 @@
 "use client"
 
+import Link from "next/link"
+import Button from "../common/components/Button"
+
 export default function Footer() {
-  return <footer></footer>
+  return (
+    <footer>
+      <div>
+        <div>
+          <h1>Sofa Society Co.</h1>
+          <p>(c) 2024, Sofa Society</p>
+        </div>
+
+        <div>
+          <Link href="/">FAQ</Link>
+          <Link href="/">Help</Link>
+          <Link href="/">Delivery</Link>
+          <Link href="/">Returns</Link>
+        </div>
+
+        <div>
+          <Link href="/">Instagram</Link>
+          <Link href="/">TikTok</Link>
+          <Link href="/">Pinterest</Link>
+          <Link href="/">Facebook</Link>
+        </div>
+
+        <div>
+          <Link href="/">Privacy Policy</Link>
+          <Link href="/">Cookie Policy</Link>
+          <Link href="/">Terms of Use</Link>
+        </div>
+
+        <div>
+          <h1>Join our newsletter</h1>
+          <p>We will also send you our discount coupons!</p>
+          <div>
+            <input placeholder="Your email" />
+            <Button text="Subscribe" />
+          </div>
+          <p>
+            By subscribing your agree with out <span>Privacy Policy</span> and
+            provide consent to receive updates from our company.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
