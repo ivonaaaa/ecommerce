fac8860b95038935349b6d977010689b328a2423
ivonaaaa <ivonaa.e@gmail.com>
2026-03-15 16:45:39 +0100

    Finish footer style; Alter button component so it accepts a classname prop for different styles

--- web/src/modules/common/components/Button.tsx
+++ web/src/modules/common/components/Button.tsx
@@ -1,6 +1,14 @@
-export default function Button({ text }: { text: string }) {
+export default function Button({
+  text,
+  className,
+}: {
+  text: string
+  className?: string
+}) {
   return (
-    <button className="bg-black rounded text-white w-full h-12 hover:bg-gray-800">
+    <button
+      className={`bg-black rounded text-white w-full h-12 hover:bg-gray-800 ${className}`}
+    >
       {text}
     </button>
   )
--- web/src/modules/footer/Footer.tsx
+++ web/src/modules/footer/Footer.tsx
@@ -9,44 +9,50 @@ export default function Footer() {
       <div className="flex flex-col lg:flex-row gap-16">
         <div className="absolute inset-0 bg-gray-100 -z-10"></div>
 
-        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-16 py-16">
-          <div>
-            <h1 className="text-4xl font-medium">Sofa Society Co.</h1>
+        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-36 py-16">
+          <div className="lg:w-2/12 pr-12">
+            <h1 className="text-4xl font-medium mb-4">Sofa Society Co.</h1>
             <p>© 2024, Sofa Society</p>
           </div>
 
-          <div className="flex gap-6">
-            <div className="flex flex-col gap-2">
+          <div className="lg:w-5/12 flex gap-20">
+            <div className="flex flex-col gap-4">
               <Link href="/">FAQ</Link>
               <Link href="/">Help</Link>
               <Link href="/">Delivery</Link>
               <Link href="/">Returns</Link>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-4">
               <Link href="/">Instagram</Link>
               <Link href="/">TikTok</Link>
               <Link href="/">Pinterest</Link>
               <Link href="/">Facebook</Link>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-4">
               <Link href="/">Privacy Policy</Link>
               <Link href="/">Cookie Policy</Link>
               <Link href="/">Terms of Use</Link>
             </div>
           </div>
 
-          <div>
-            <h1>Join our newsletter</h1>
+          <div className="lg:w-5/12">
+            <h1 className="text-3xl font-medium mb-2">Join our newsletter</h1>
             <p>We will also send you our discount coupons!</p>
-            <div>
-              <input placeholder="Your email" />
-              <Button text="Subscribe" />
+            <div className="flex gap-2 my-3">
+              <input
+                placeholder="Your email"
+                className="text-sm w-9/12 rounded px-4"
+              />
+              <div className="w-3/12">
+                <Button text="Subscribe" className="text-sm" />
+              </div>
             </div>
-            <p>
-              By subscribing your agree with out <span>Privacy Policy</span> and
-              provide consent to receive updates from our company.
+            <p className="text-sm text-gray-400">
+              By subscribing your agree with out{" "}
+              <span className="underline cursor-pointer">Privacy Policy</span>{" "}
+              and provide consent to receive updates from our company.
             </p>
           </div>
         </div>
